79095313a25a3ec773c95f192b085bd03d605e26
Haskell <austinoliverhaskell@gmail.com>
2023-01-08 15:07:48 -0600

    Misunderstood Bork on what he wanted

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,26 @@ fn main() {
     let mut pin25 = gpio.get(25).unwrap().into_output();
 
     loop {
-        println!("Setting High!");
+        println!("Start Cycle");
 
 
 
         pin22.set_high();
         thread::sleep(Duration::from_millis(500));
+        pin22.set_low();
 
         pin23.set_high();
         thread::sleep(Duration::from_millis(500));
+        pin23.set_low();
 
         pin24.set_high();
         thread::sleep(Duration::from_millis(500));
+        pin24.set_low();
 
         pin25.set_high();
         thread::sleep(Duration::from_millis(500));
-
-        println!("Setting Low!");
-        pin22.set_low();
-        pin23.set_low();
-        pin24.set_low();
         pin25.set_low();
+
 
         thread::sleep(Duration::from_millis(500));
     }
